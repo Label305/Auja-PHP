8c29009f3f54f261ed2634a568289463eaf320c1
Niek Haarman <niek@label305.com>
2014-10-31 09:44:10 +0100

    Created Property and Searchable.

---
+++ spec/Label305/Auja/Menu/Property/PropertySpec.php
@@ -0,0 +1,72 @@
+<?php
+/*   _            _          _ ____   ___  _____
+ *  | |          | |        | |___ \ / _ \| ____|
+ *  | |      __ _| |__   ___| | __) | | | | |__
+ *  | |     / _` | '_ \ / _ \ ||__ <|  -  |___ \
+ *  | |____| (_| | |_) |  __/ |___) |     |___) |
+ *  |______|\__,_|_.__/ \___|_|____/ \___/|____/
+ *
+ *  Copyright Label305 B.V. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace spec\Label305\Auja\Menu\Property;
+
+set_include_path(dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.PATH_SEPARATOR.get_include_path());
+require_once('BaseSpec.php'); // TODO: can this be prettier?
+
+use Label305\Auja\Menu\Property\Property;
+use spec\Label305\Auja\BaseSpec;
+
+class PropertySpec extends BaseSpec {
+
+    const TARGET = 'target';
+
+    function let() {
+        $this->beAnInstanceOf('spec\Label305\Auja\Menu\Property\DummyProperty');
+        $this->beConstructedWith(self::TARGET);
+    }
+
+    function it_is_initializable() {
+        $this->shouldHaveType('Label305\Auja\AujaItem');
+        $this->shouldHaveType('Label305\Auja\Menu\Property\Property');
+    }
+
+    function it_has_a_target() {
+        $this->getTarget()->shouldBe(self::TARGET);
+        $this->setTarget('New target');
+        $this->getTarget()->shouldBe('New target');
+    }
+
+    function it_can_return_json_serializable_data() {
+        $this->jsonSerialize()->shouldBeArray();
+        $this->jsonSerialize()->shouldHaveCount(1);
+        $this->jsonSerialize()->shouldHaveKey('dummy');
+
+        $result = $this->jsonSerialize()->getWrappedObject();
+        if($result['dummy']['target'] != self::TARGET){
+            throw new \Exception('Wrong target');
+        }
+    }
+}
+
+class DummyProperty extends Property {
+
+    /**
+     * @return String The type of the `AujaItem`.
+     */
+    public function getType() {
+        return 'dummy';
+    }
+}
---
+++ src/Label305/Auja/Menu/Property/Searchable.php
@@ -0,0 +1,76 @@
+<?php
+/*   _            _          _ ____   ___  _____
+ *  | |          | |        | |___ \ / _ \| ____|
+ *  | |      __ _| |__   ___| | __) | | | | |__
+ *  | |     / _` | '_ \ / _ \ ||__ <|  -  |___ \
+ *  | |____| (_| | |_) |  __/ |___) |     |___) |
+ *  |______|\__,_|_.__/ \___|_|____/ \___/|____/
+ *
+ *  Copyright Label305 B.V. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Label305\Auja\Menu\Property;
+
+/**
+ * A class which represents the Searchable property.
+ *
+ * @author  Niek Haarman - <niek@label305.com>
+ *
+ * @package Label305\Auja\Menu\Property
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+class Searchable extends Property {
+
+    const TYPE = 'searchable';
+
+    /**
+     * Creates a new searchable property with given target.
+     *
+     * @param String $target The target url. It should contain '%s' where the search query should go.
+     *
+     * @throws \InvalidArgumentException if the target is invalid.
+     */
+    function __construct($target) {
+        $this->verifyTarget($target);
+        parent::__construct($target);
+    }
+
+    /**
+     * @param String $target The target url. It should contain '%s' where the search query should go.
+     *
+     * @throws \InvalidArgumentException if the target is invalid.
+     */
+    public function setTarget($target) {
+        $this->verifyTarget($target);
+        parent::setTarget($target);
+    }
+
+    public function getType() {
+        return self::TYPE;
+    }
+
+    /**
+     * Verifies whether given target url contains '%s'.
+     *
+     * @param String $target The target url which should contain '%s'.
+     *
+     * @throws \InvalidArgumentException if the target is invalid.
+     */
+    private function verifyTarget($target) {
+        if (strpos($target, '%s') === false) {
+            throw new \InvalidArgumentException('Searchable target url should contain \'%s\'.');
+        }
+    }
+}
